33053b40484aaa2a3f95aab7ebe7e39e4ce6aafc
MassimoBergantin <maxbergax@gmail.com>
2021-06-03 15:55:00 +0200

    cognomi

---
+++ js/script.js
@@ -0,0 +1,24 @@
+
+
+
+// 1. chiedi all’utente il cognome
+// 2. inseriscilo in un array dove sono presenti già altri cognomi: ‘Bianchi’, ‘Neri’, ‘Rossi’, ‘Verdi’, ‘Gialli’
+// 3. stampa la lista ordinata alfabeticamente
+// 4. scrivi anche la posizione “umana” (partendo da 1) della lista in cui il nuovo utente si trova
+
+
+
+
+var usersurname = prompt("Dimmi il tuo cognome");
+var listacognomi = ['Zanella','Verdi','Neri','Bossi','Alfonsi'];
+
+listacognomi.push(usersurname);
+
+document.getElementById("output").innerHTML = listacognomi;
+
+function myFunction() { 
+  listacognomi.sort();
+  document.getElementById("output").innerHTML = listacognomi;
+}
+
+
